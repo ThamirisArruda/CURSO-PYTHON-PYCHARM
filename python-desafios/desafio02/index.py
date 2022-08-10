2031d06720f3bb43a97d13c8f15cb48d96d23e75
thamiris_arruda <thamydyanara@gmail.com>
2022-08-10 18:12:50 -0300

    envio de teste python

---
+++ python-desafios/desafio02/index.py
@@ -0,0 +1,4 @@
+Dia=input('Dia=')
+Mês=input('Mês=')
+Ano=input('Ano=')
+print('Você nasceu no dia',Dia, 'de' ,Mês, 'de' ,Ano,)
---
+++ python-desafios/desafio03/index.py
@@ -0,0 +1,4 @@
+numero1=input('Primeiro Número:')
+numero2=input('Segundo Número:')
+soma=int (numero1)+int (numero2)
+print('Resultado:',soma)
