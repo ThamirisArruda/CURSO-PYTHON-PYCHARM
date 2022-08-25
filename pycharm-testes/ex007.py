e16a7ece93386e42a8d45fbd48e9aafa3ee3d414
thamiris_arruda <thamydyanara@gmail.com>
2022-08-25 17:44:43 -0300

    adiciona exercicios novos

---
+++ pycharm-testes/ex007.py
@@ -0,0 +1,6 @@
+n = int(input("Digite um número: "))
+a = n-1
+s = n+1
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}' .format(n, a, s))
+#n= int(input)('Digite um número'))
+#print('Analisando o valor {}, seu antecessor é {}e o sucessor é {}' .format(n,(n-1), (n+1)))
---
+++ pycharm-testes/ex011.py
@@ -0,0 +1,4 @@
+medida = float(input('Uma distância em metros: '))
+hm = medida / 100
+km = medida / 1000
+print('A medida de {}m corresponde a {}hm e {}km' .format(medida, hm, km))
