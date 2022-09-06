605f18694d647c12b836c55f18146e0886cf6247
thamiris_arruda <thamydyanara@gmail.com>
2022-09-06 18:00:24 -0300

    adiciona modificação de exer e exer novos

---
+++ pycharm-testes/python3-mundo01.1/ex001.py
@@ -0,0 +1,5 @@
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}' .format (num, trunc(num)))
+#num = float (input('Digite um valor: '))
+#print('O valor digitado foi {} e sua porção inteira é {} .format(num, int(num)))
