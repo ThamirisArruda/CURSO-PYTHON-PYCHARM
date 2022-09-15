49c2ea67159e387bd8461fa4ff113f81c7644193
thamiris_arruda <thamydyanara@gmail.com>
2022-09-15 16:41:11 -0300

    adiciona ex

---
+++ pycharm-testes/python3-mundo01.1/ex008.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu nome: ')).strip()
+print('Analisando o seu nome...')
+print('Seu nome em maiúsculas é {}' .format(nome.upper()))
+print('Seu nome em minúsculas é {}' .format(nome.lower()))
+print('Seu nome ao todo {} letras' .format(len(nome) - nome.count(' ')))
+print('Seu primeiro nome tem {}' .format((nome.find (' ')))
